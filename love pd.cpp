2674bee1871f42efb9a012eb4f1d791968b439a4
AnYYYuan <104673884+AAAnYuan@users.noreply.github.com>
2022-10-01 11:45:58 +0800

    Update love pd.cpp

--- love pd.cpp
+++ love pd.cpp
@@ -1,20 +1,36 @@
-﻿// ConsoleApplication2.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-
-#include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n";
+#include<stdio.h>
+#include <windows.h>
+#include<tchar.h>
+#include<iostream>
+#include<string>
+using namespace std;
+string str1, str2;
+int main() {
+    srand((unsigned)time(NULL));
+    double x, y, a;
+    printf("感情判断器\n");
+    printf("请输入两个名字\n");
+    cin >> str1 >> str2;
+    cout << "                      " << str1 << " and " << str2 << "\n";
+    int pd = rand() % 10;
+    if (pd == 1 or pd == 5 or pd == 4 or pd == 9 or pd == 3)
+        system("color F2");
+    else
+        system("color F4");
+    for (y = 1.5; y > -1.5; y -= 0.1214)
+    {
+        for (x = -1.5; x < 1.5; x += 0.05)
+        {
+            a = x * x + y * y - 1;
+            if (a * a * a - x * x * y * y * y <= 0)
+            {
+                printf("*");
+                Sleep(1);
+            }
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+    return 0;
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
